c60890956014241f6987861a01f70a6224143385
Keiba-coder <gabrieljn2008@gmail.com>
2026-03-19 22:28:48 -0300

    Aula 2 de variáveis e operadores

---
+++ Aula_02Variaveis/Aula_Variaveis.py
@@ -0,0 +1,25 @@
+print("Olá mundo")
+print(7+4)
+print("7"+"4") # concatenação de String
+
+'''
+Muito texto msm
+'''
+print("Foda isso daí")
+
+#
+'''
+Keiba #string
+idade = 67 #int
+peso = 67 #float
+print(nome, idade, peso)
+print(f"fala aí, {nome}!!!!")
+'''
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+peso = input("Digite seu peso: ")
+print(nome, idade, peso)
+
+nova_idade = (idade + 1)
+print(nova_idade)
